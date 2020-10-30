042259951b693ecb19d6e37fda6dfc455add2473
Paulo Castelo <oluap.castelo@gmail.com>
2020-10-29 23:33:56 -0300

    Section #13, Chapter #189: StramWriter.

---
+++ Section13/StreamWriterFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace StreamWriterFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Projetos\file1.txt";
+            string targetPath = @"C:\Projetos\file2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string s in lines)
+                    {
+                        sw.WriteLine(s.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
