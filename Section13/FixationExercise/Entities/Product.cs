eed1cc4a770f0f174d25acc1a845a456c8858b8d
Paulo Castelo <oluap.castelo@gmail.com>
2020-10-31 00:39:42 -0300

    Section #13, Chapter #192: Fixation Exercises

---
+++ Section13/FixationExercise/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace FixationExercise.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+        public Product() { }
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + TotalPrice().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Section13/FixationExercise/Program.cs
@@ -0,0 +1,92 @@
+﻿using FixationExercise.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace FixationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootFolderPath = @"C:\Projetos\Files";
+            string sourceFileCsv = rootFolderPath + @"\input\source.csv";
+            string sourceFolderPath = Path.GetDirectoryName(sourceFileCsv);
+            string targetFolderPath = rootFolderPath + @"\output";
+            string targetFileCSV = targetFolderPath + @"\target.csv";
+
+            if (!Directory.Exists(rootFolderPath))
+            {
+                Directory.CreateDirectory(rootFolderPath);
+                Directory.CreateDirectory(sourceFolderPath);
+                Directory.CreateDirectory(targetFolderPath);
+            }
+
+            List<Product> list = new List<Product>();
+
+            try
+            {
+                if (!File.Exists(sourceFileCsv))
+                {
+                    Console.WriteLine("Creating source file. . .");
+                    string[] text ={ "TV LED, 1290.99, 1"
+                    ,"Video Game Chair, 350.50, 3"
+                    ,"Iphone X, 900.00, 2"
+                    ,"Samsung Galaxy 9, 850.00, 2" };
+
+                    FileInfo newFile = new FileInfo(sourceFileCsv);
+                    using (StreamWriter outputSourceFile = new StreamWriter(Path.Combine(sourceFileCsv)))
+                    {
+                        foreach (string line in text)
+                        {
+                            outputSourceFile.WriteLine(line);
+                        }
+                    }
+                    Console.WriteLine("Source file created!");
+                }
+
+                if (File.Exists(targetFileCSV))
+                {
+                    File.Delete(targetFileCSV);
+                }
+
+                string[] item = File.ReadAllLines(sourceFileCsv);
+                Console.WriteLine("Source File:");
+                foreach (string s in item)
+                {
+                    Console.WriteLine(s);
+                    string[] addItem = s.Split(", ");
+                    string name = addItem[0];
+                    double price = double.Parse(addItem[1], CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(addItem[2]);
+                    list.Add(new Product(name, price, quantity));
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Creating targetFile. . .");
+                Console.WriteLine("Target file:");
+                using (StreamWriter outputDestinyCSV = new StreamWriter(Path.Combine(targetFileCSV)))
+                {
+                    foreach (Product product in list)
+                    {
+                        Console.WriteLine(product);
+                        outputDestinyCSV.WriteLine(product);
+                    }
+                }
+                Console.WriteLine("Target file created!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred!");
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error has occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
