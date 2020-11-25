54ffc06a1036ac4607e405a4fc19280e2174eca7
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-24 23:00:35 -0300

    Commit changes. Section15 Chapter#205: Generics Part II.

--- Section15/Section15Generics/Program.cs
+++ Section15/Section15Generics/Program.cs
@@ -12,7 +12,7 @@ namespace Section15Generics
 
             for (int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
+                string x = Console.ReadLine();
                 printService.AddValue(x);
             }
 
