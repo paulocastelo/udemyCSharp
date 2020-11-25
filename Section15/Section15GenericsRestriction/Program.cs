0aeb2ccbceae982dd4b4b8bc1457e089a0454103
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-24 23:50:11 -0300

    Commit changes. Section15 Chapter#206: Generic's restrinctions

---
+++ Section15/Section15GenericsRestriction/Program.cs
@@ -0,0 +1,34 @@
+﻿using Section15GenericsRestriction.Entities;
+using Section15GenericsRestriction.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Section15GenericsRestriction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter Number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(',');
+                string name = values[0];
+                double price = double.Parse(values[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
