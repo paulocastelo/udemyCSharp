16f9f8b6fdc08365daee4975f68351d14c6eae05
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-24 22:26:25 -0300

    Commit changes. Section15 Chapter#204: Generics Part I

---
+++ Section15/Section15Generics/PrintService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section15Generics
+{
+    class PrintService
+    {
+        private int[] _values = new int[10];
+        public int _count = 0;
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full!");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+        public int first()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty!");
+            }
+            return _values[0];
+        }
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
