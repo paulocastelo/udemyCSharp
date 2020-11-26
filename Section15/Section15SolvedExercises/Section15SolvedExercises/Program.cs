145ac0e5901f0e3567e648cd9946c6635bb63563
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 17:30:06 -0300

    Commit changes. Section15 Chapter#211: Solved exercises of ser=ts

---
+++ Section15/Section15SolvedExercises/Section15SolvedExercises/Program.cs
@@ -0,0 +1,33 @@
+﻿using Section15SolvedExercises.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Section15SolvedExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the file full path: ");
+            string path = Console.ReadLine();
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vect = sr.ReadLine().Split(" ");
+                        set.Add(new LogRecord(vect[0],DateTime.Parse(vect[1])));
+                    }
+                }
+                Console.WriteLine("Total users: " + set.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
