5b7554ad93b56612122daf052d7b8e3f4139da3c
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 20:13:22 -0300

    Commit changes. Section16 Chapters#215, #216 and 217 (Extension Methods).

---
+++ Section16/Section16ExtensionMethods/Extensions/DateTimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section16ExtensionMethods.Extensions
+{
+    static class DateTimeExtension
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if(duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else if (duration.TotalDays < 7)
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+            else
+            {
+                double s = duration.TotalDays / 7;
+                return s.ToString("F1", CultureInfo.InvariantCulture) + " weeks";
+            }
+        }
+    }
+}
---
+++ Section16/Section16ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Section16ExtensionMethods.Extensions;
+
+namespace Section16ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 01, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+            string s1 = "Good morning dear students";
+            Console.WriteLine(s1.Cut(15));
+        }
+    }
+}
