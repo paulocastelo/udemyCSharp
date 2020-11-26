db5d6ec0ca80f56d46ac4763baef77ef9e3f6cb9
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 18:27:06 -0300

    Commit changes. Section15 Chapter#212: Proposed exercise

---
+++ Section15/Section15ProposedExercise/Section15ProposedExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section15ProposedExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int>();
+            HashSet<int> b = new HashSet<int>();
+            HashSet<int> c = new HashSet<int>();
+            Console.Write("How many students for course A?");
+            int na = int.Parse(Console.ReadLine());
+            for (int i = 0; i < na; i++)
+            {
+                a.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.Write("How many students for course B?");
+            int nb = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nb; i++)
+            {
+                b.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.Write("How many students for course C?");
+            int nc = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nc; i++)
+            {
+                c.Add(int.Parse(Console.ReadLine()));
+            }
+            HashSet<int> d = new HashSet<int>(a);
+            d.UnionWith(b);
+            d.UnionWith(c);
+            Console.WriteLine("Total students: " + d.Count);
+        }
+    }
+}
