bb817e3465f4ae614993a94de243f8f035fc8d27
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-25 23:15:11 -0300

    Commit changes. Chapter#207: Equals and GetHashCode

---
+++ Section15/Section15GetHashCodeAndEquals/Section15GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Section15GetHashCodeAndEquals.Entities;
+
+namespace Section15GetHashCodeAndEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a = "Maria";
+            //string b = "Alex";
+            //string c = "Maria";
+
+            //Console.WriteLine(c.GetHashCode());
+            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client() { Name = "Alex", Email = "alex@gmail.com" };
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
