2bca975794055fa25de2e2cdf58f78cfa0d10248
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 20:10:23 -0300

    Commit changes. Section15 Chapter#214: Dictionary proposed exercise

---
+++ Section15/Section15DictionaryProposedExercise/Section15DictionaryProposedExercise/Entities/Candidate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section15DictionaryProposedExercise.Entities
+{
+    class Candidate
+    {
+        public string Name { get; set; }
+        public int Vote { get; set; }
+        public Candidate(string name, int vote)
+        {
+            Name = name;
+            Vote = vote;
+        }
+        //public override bool Equals(object obj)
+        //{
+        //    if (!(obj is Candidate))
+        //    {
+        //        return false;
+        //    }
+        //    Candidate other = obj as Candidate;
+        //    return Name.Equals(other.Name);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return Name.GetHashCode();
+        //}
+    }
+}
---
+++ Section15/Section15DictionaryProposedExercise/Section15DictionaryProposedExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Section15DictionaryProposedExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file full path: ");
+            string test = Console.ReadLine();
+            string path = @"C:\Projetos\UdemyCSharp\Section15DictionaryProposedExercise\Section15DictionaryProposedExercise\in.txt";
+            Dictionary<string, int> votingRecord = new Dictionary<string, int>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vect = sr.ReadLine().Split(",");
+                        string name = vect[0];
+                        int vote = int.Parse(vect[1]);
+                        if (votingRecord.ContainsKey(name))
+                        {
+                            votingRecord[name] += vote;
+                        }
+                        else
+                        {
+                            votingRecord.Add(name, vote);
+                        }
+                    }
+                }
+                foreach (var candidate in votingRecord)
+                {
+                    Console.WriteLine(candidate.Key + ", " + candidate.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
