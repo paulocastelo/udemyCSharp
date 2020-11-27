1c44820bf9805068f0d837e5253282c7acfeae1a
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 23:48:09 -0300

    Commit changes. Section17 Chapter#226: Introducting LINQ.

---
+++ Section17/Section17IntroductingLINQ/Section17IntroductingLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Section17IntroductingLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            var result = numbers
+                .Where(X => X % 2 == 0)
+                .Select(x => x * 10);
+            //Execute the query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
