95489829f660d1e04130947d9c194c2d41cdf277
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 23:06:14 -0300

    Commit changes. Section17 Chapter#224: Delegate Action with foreach.

---
+++ Section17/Section17DelegateActionWithForeach/Section17DelegateActionWithForeach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Section17DelegateActionWithForeach.Entities;
+
+
+namespace Section17DelegateActionWithForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            list.Sort();
+
+            Action<Product> act = PrintProduct;
+            list.ForEach(act);
+
+        }
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+
+        static void PrintProduct(Product p)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
