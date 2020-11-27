414109df55802498dbfe455ecf41af1778f694ac
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 23:32:32 -0300

    Commit changes. Section17 Chapter#225: Delegate Func with Select.

---
+++ Section17/Section17DelegateFuncWithSelect/Section17DelegateFuncWithSelect/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text;
+
+namespace Section17DelegateFuncWithSelect.Entities
+{
+    class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("The object is not a product.");
+            }
+            Product other = obj as Product;
+            return Name.ToUpper().Equals(other.Name.ToUpper());
+        }
+        public int CompareTo(Product other)
+        {
+            if (!(other is Product))
+            {
+                throw new ArgumentException("The object is not a product.");
+            }
+            Product p = other as Product;
+            return Name.ToUpper().CompareTo(p.Name.ToUpper());
+        }
+    }
+}
+
---
+++ Section17/Section17DelegateFuncWithSelect/Section17DelegateFuncWithSelect/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Section17DelegateFuncWithSelect.Entities;
+
+namespace Section17DelegateFuncWithSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD case", 80.90));
+
+            list.ForEach(PrintProduct);
+
+            Func<Product, string> func = NameUpper;
+            List<string> result = list.Select(func).ToList();
+            
+            Console.WriteLine("\nUsing Lambda in line: ");
+            result.ForEach(p => Console.WriteLine(p));
+
+            Console.WriteLine("\nUsing Action:");
+            Action<string> act = PrintString;
+            result.ForEach(act);
+        }
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+        static void PrintString(string p)
+        {
+            Console.WriteLine(p);
+        }
+        static void PrintProduct(Product p)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
