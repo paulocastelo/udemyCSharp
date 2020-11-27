b60067cae3c409eceeb1cd2b526997a3cd73a672
Paulo Castelo <oluap.castelo@gmail.com>
2020-11-26 22:20:16 -0300

    Commit changes. Chapter#222: Multicast Delegate.

---
+++ Section17/Section17MultiCastDelegate/Section17MultiCastDelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Section17MultiCastDelegate.Services;
+
+namespace Section17MultiCastDelegate
+{
+    delegate void BinaryNuericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNuericOperation op = CalculationServices.ShowMax;
+            op += CalculationServices.ShowSum;
+            op(a, b);
+        }
+    }
+}
---
+++ Section17/Section17MultiCastDelegate/Section17MultiCastDelegate/Services/CalculationServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Section17MultiCastDelegate.Services
+{
+    class CalculationServices
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine("Max: " + max);
+        }
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
